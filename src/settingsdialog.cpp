34113c25f9628b06c21052663fc38b24f9e95f30
Yani <yani@protonmail.com>
2026-07-08 10:25:07 +0200

    make ui sliders in settings enable the save button on change
    (cherry picked from commit 8acb86775e3fe8c597cc299bb2f180d78345144d)

--- src/settingsdialog.cpp
+++ src/settingsdialog.cpp
@@ -1181,6 +1181,15 @@ void SettingsDialog::addSettingsChangedHandler()
         });
     }
 
+    // Find all QSlider
+    QList<QSlider *> sliders = ui->tabWidget->findChildren<QSlider *>();
+    for (QSlider *slider : std::as_const(sliders)) {
+        connect(slider, &QSlider::sliderMoved, this, [this]() {
+            this->settingHasChanged = true;
+            ui->buttonBox->button(QDialogButtonBox::Save)->setDisabled(false);
+        });
+    }
+
     // Find all PopupSignalComboBox
     QList<PopupSignalComboBox *> popupComboBoxes = ui->tabWidget->findChildren<PopupSignalComboBox *>();
     for (PopupSignalComboBox *popupComboBox : std::as_const(popupComboBoxes)) {
