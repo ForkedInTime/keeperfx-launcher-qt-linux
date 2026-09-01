ebe73c8c623c98bd36b62808afae92fcdb140151
Yani <yani@protonmail.com>
2026-09-01 11:02:20 +0200

    update zoom-to-mouse config option
    (cherry picked from commit 2bb926dbe457ff0ecfeeeaa293cb0734ed30fcce)

--- src/kfxversion.cpp
+++ src/kfxversion.cpp
@@ -30,7 +30,7 @@ const QMap<QString, QString> KfxVersion::versionFunctionaltyMap = {
     {"save_file_struct_lua", "1.2.0.4479"},
     {"save_file_struct_30_char_name", "1.3.1.4881"},
     {"mouse_sensitivity_no_multiplier", "1.3.2.5120"},
-    {"zoom_towards_mouse", "1.3.2.5134"},
+    {"zoom_towards_mouse", "1.4.0.4323"},
     {"rotate_around_mouse", "1.4.0.4323"},
 
     // Absolute Config path is temporary disabled because we still want support for multiple KFX installations
